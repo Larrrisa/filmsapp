a786b69fd89ccc04799a8d22cb8827ebfae39e7d
Larisa Nikonova <larisa.n@itfox-web.com>
2026-03-12 22:39:04 +0400

    fix: update colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import { getFilms } from "@/api/api";
+
 export default function Home() {
-  return <h1>Home</h1>
+  const { filmsData, isFilmsDataLoading } = getFilms(1);
+  // const { seriesData, isSeriesDataLoading } = getSeries(1);
+  // const { trendingFilmsData, isTrendingFilmsDataLoading } = getTrendingFilms(1);
+
+  return (
+     <>
+         <h2>
+           Popular films
+         </h2>
+         {/*<Link to="/films">
+           <ArrowIconRight />
+         </Link>*/}
+
+       <div>
+         {isFilmsDataLoading ? (
+         <p>Loading....</p>
+         ) : (
+         <p>Films</p>
+         )}
+       </div>
+
+     </>
+   );
 }
--- pages/films/index.tsx
+++ pages/films/index.tsx
@@ -2,7 +2,7 @@ import { getFilms } from "@/api/api";
 
 export default function FilmsPage() {
 
-  const filmsData = getFilms(1 );
+  const filmsData = getFilms(1);
 
   return <div>FilmsPage</div>
 }
